429a147992992d52fbde66918c57671831ed5d92
siclol <marko.milky@gmail.com>
2019-08-08 13:50:24 +0200

    feat(o-nama): done

--- src/routes/o-nama.svelte
+++ src/routes/o-nama.svelte
@@ -2,4 +2,293 @@
 	<title>CZA Osijek | O nama</title>
 </svelte:head>
 
-<p>o-nama</p>
+<!--O nama-->
+<section class="grid p-y-xl">
+	<div class="col-6 col-m-8 col-s-10 col-xs-12">
+		<div class="read-format">
+			<h1>O nama</h1>
+			<p><b>Naša misija</b> je suradnjom nastavnika, roditelja i lokalne zajednice svakom djetetu omogućiti razvitak u sredini u kojoj će se osjećati prihvaćeno, voljeno, sigurno i zadovoljno. Kreirati sredinu koja će djelovati poticajno na razvitak svih njegovih potencijala, u kojoj će se voditi briga o djetetovim specifičnostima kao pojedinca i gdje će ga se osposobljavati za samostalan i odgovoran život.</p>
+			<p><b>Naša vizija</b> je odgoj i obrazovanje sretnog i uspješnog učenika koji se razvija uz poticaj učitelja, podršku roditelja i cijele lokalne zajednice.</p>
+			<p><b>Naše vrijednosti</b> su sljedeće:</p>
+			<ul>
+				<li>Strastveni smo prema ljudima i pozitivni o tome što je moguće.</li>
+				<li>Ljudi su na prvom mjestu.</li>
+				<li>Cijenimo zajednički rad.</li>
+				<li>Slavimo raznolikost.</li>
+				<li>Uživamo u jedinstvenosti.</li>
+				<li>Težimo većim stvarima.</li>
+				<li>Gledamo u budućnost.</li>
+				<li>Usmjereni smo na naše snage.</li>
+			</ul>
+		</div>
+	</div>
+</section>
+
+<!--Naš tim-->
+<section class="grid">
+	<div class="col-6 col-m-8 col-s-10 col-xs-12 read-format">
+		<h1>Naš tim</h1>
+	</div>
+</section>
+<section class="grid p-b-xl m-b-l">
+	<div class="col-9 col-m-10 col-s-11 col-xs-12 p-a-0">
+		<section class="grid">
+			<div class="col-3 col-m-4 col-s-6 col-xs-12 p-b-l ta-center">
+				<picture>
+					<source srcset="assets/images/o-nama/sanda-zlataric.webp" type="image/webp">
+					<source srcset="assets/images/o-nama/sanda-zlataric.png" type="image/png">
+					<img src="assets/images/o-nama/sanda-zlataric.png" alt="Sanda Zlatarić">
+				</picture>
+				<h4 class="m-t-m m-b-xs">Sanda Zlatarić</h4>
+				<p class="fg-secondary fs-small">Ravnateljica</p>
+			</div>
+			<div class="col-3 col-m-4 col-s-6 col-xs-12 p-b-l ta-center">
+				<picture>
+					<source srcset="assets/images/o-nama/zvonimir-borevac.webp" type="image/webp">
+					<source srcset="assets/images/o-nama/zvonimir-borevac.png" type="image/png">
+					<img src="assets/images/o-nama/zvonimir-borevac.png" alt="Zvonimir Borevac">
+				</picture>
+				<h4 class="m-t-m m-b-xs">Zvonimir Borevac</h4>
+				<p class="fg-secondary fs-small">Tajnik</p>
+			</div>
+			<div class="col-3 col-m-4 col-s-6 col-xs-12 p-b-l ta-center">
+				<picture>
+					<source srcset="assets/images/o-nama/ksenija-sudar.webp" type="image/webp">
+					<source srcset="assets/images/o-nama/ksenija-sudar.png" type="image/png">
+					<img src="assets/images/o-nama/ksenija-sudar.png" alt="Ksenija Sudar">
+				</picture>
+				<h4 class="m-t-m m-b-xs">Ksenija Sudar</h4>
+				<p class="fg-secondary fs-small">Voditelj Računovodstva</p>
+			</div>
+			<div class="col-3 col-m-4 col-s-6 col-xs-12 p-b-l ta-center">
+				<picture>
+					<source srcset="assets/images/o-nama/elizabeta-hanicar.webp" type="image/webp">
+					<source srcset="assets/images/o-nama/elizabeta-hanicar.png" type="image/png">
+					<img src="assets/images/o-nama/elizabeta-hanicar.png" alt="Elizabeta Haničar">
+				</picture>
+				<h4 class="m-t-m m-b-xs">Elizabeta Haničar</h4>
+				<p class="fg-secondary fs-small">Pedagog</p>
+			</div>
+			<div class="col-3 col-m-4 col-s-6 col-xs-12 p-b-l ta-center">
+				<picture>
+					<source srcset="assets/images/o-nama/katarina-berki.webp" type="image/webp">
+					<source srcset="assets/images/o-nama/katarina-berki.png" type="image/png">
+					<img src="assets/images/o-nama/katarina-berki.png" alt="Katarina Berki">
+				</picture>
+				<h4 class="m-t-m m-b-xs">Katarina Berki</h4>
+				<p class="fg-secondary fs-small">Kuharica</p>
+			</div>
+			<div class="col-3 col-m-4 col-s-6 col-xs-12 p-b-l ta-center">
+				<picture>
+					<source srcset="assets/images/o-nama/slaven-ernjes.webp" type="image/webp">
+					<source srcset="assets/images/o-nama/slaven-ernjes.png" type="image/png">
+					<img src="assets/images/o-nama/slaven-ernjes.png" alt="Slaven Ernješ">
+				</picture>
+				<h4 class="m-t-m m-b-xs">Slaven Ernješ</h4>
+				<p class="fg-secondary fs-small">Domar</p>
+			</div>
+			<div class="col-3 col-m-4 col-s-6 col-xs-12 p-b-l ta-center">
+				<picture>
+					<source srcset="assets/images/o-nama/drazenka-pralija.webp" type="image/webp">
+					<source srcset="assets/images/o-nama/drazenka-pralija.png" type="image/png">
+					<img src="assets/images/o-nama/drazenka-pralija.png" alt="Draženka Pralija">
+				</picture>
+				<h4 class="m-t-m m-b-xs">Draženka Pralija</h4>
+				<p class="fg-secondary fs-small">Spremačica</p>
+			</div>
+			<div class="col-3 col-m-4 col-s-6 col-xs-12 p-b-l ta-center">
+				<picture>
+					<source srcset="assets/images/o-nama/marijana-fiala.webp" type="image/webp">
+					<source srcset="assets/images/o-nama/marijana-fiala.png" type="image/png">
+					<img src="assets/images/o-nama/marijana-fiala.png" alt="Marijana Fiala">
+				</picture>
+				<h4 class="m-t-m m-b-xs">Marijana Fiala</h4>
+				<p class="fg-secondary fs-small">Spremačica</p>
+			</div>
+			<div class="col-3 col-m-4 col-s-6 col-xs-12 p-b-l ta-center">
+				<picture>
+					<source srcset="assets/images/o-nama/nada-varzic.webp" type="image/webp">
+					<source srcset="assets/images/o-nama/nada-varzic.png" type="image/png">
+					<img src="assets/images/o-nama/nada-varzic.png" alt="Nada Varžić">
+				</picture>
+				<h4 class="m-t-m m-b-xs">Nada Varžić</h4>
+				<p class="fg-secondary fs-small">Spremačica</p>
+			</div>
+			<div class="col-3 col-m-4 col-s-6 col-xs-12 p-b-l ta-center">
+				<picture>
+					<source srcset="assets/images/o-nama/marija-horvat.webp" type="image/webp">
+					<source srcset="assets/images/o-nama/marija-horvat.png" type="image/png">
+					<img src="assets/images/o-nama/marija-horvat.png" alt="Marija Horvat">
+				</picture>
+				<h4 class="m-t-m m-b-xs">Marija Horvat</h4>
+				<p class="fg-secondary fs-small">Spremačica</p>
+			</div>
+			<div class="col-3 col-m-4 col-s-6 col-xs-12 p-b-l ta-center">
+				<picture>
+					<source srcset="assets/images/o-nama/marija-mrđa.webp" type="image/webp">
+					<source srcset="assets/images/o-nama/marija-mrđa.png" type="image/png">
+					<img src="assets/images/o-nama/marija-mrđa.png" alt="Marija Mrđa">
+				</picture>
+				<h4 class="m-t-m m-b-xs">Marija Mrđa</h4>
+				<p class="fg-secondary fs-small">Medicinska sestra</p>
+			</div>
+			<div class="col-3 col-m-4 col-s-6 col-xs-12 p-b-l ta-center">
+				<picture>
+					<source srcset="assets/images/o-nama/suzana-matic.webp" type="image/webp">
+					<source srcset="assets/images/o-nama/suzana-matic.png" type="image/png">
+					<img src="assets/images/o-nama/suzana-matic.png" alt="Suzana Matić">
+				</picture>
+				<h4 class="m-t-m m-b-xs">Suzana Matić</h4>
+				<p class="fg-secondary fs-small">Njegovateljica</p>
+			</div>
+			<div class="col-3 col-m-4 col-s-6 col-xs-12 p-b-l ta-center">
+				<picture>
+					<source srcset="assets/images/o-nama/mia-basic.webp" type="image/webp">
+					<source srcset="assets/images/o-nama/mia-basic.png" type="image/png">
+					<img src="assets/images/o-nama/mia-basic.png" alt="Mia Bašić">
+				</picture>
+				<h4 class="m-t-m m-b-xs">Mia Bašić</h4>
+				<p class="fg-secondary fs-small">Učitelj edukacijski rehabilitator u produženom stručnom postupku i rehabilitacijskim programima</p>
+			</div>
+			<div class="col-3 col-m-4 col-s-6 col-xs-12 p-b-l ta-center">
+				<picture>
+					<source srcset="assets/images/o-nama/karolina-burek-bilokapic.webp" type="image/webp">
+					<source srcset="assets/images/o-nama/karolina-burek-bilokapic.png" type="image/png">
+					<img src="assets/images/o-nama/karolina-burek-bilokapic.png" alt="Karolina Burek Bilokapić">
+				</picture>
+				<h4 class="m-t-m m-b-xs">Karolina Burek Bilokapić</h4>
+				<p class="fg-secondary fs-small">Učitelj edukacijski rehabilitator u produženom stručnom postupku i rehabilitacijskim programima</p>
+			</div>
+			<div class="col-3 col-m-4 col-s-6 col-xs-12 p-b-l ta-center">
+				<picture>
+					<source srcset="assets/images/o-nama/dora-marjanovic.webp" type="image/webp">
+					<source srcset="assets/images/o-nama/dora-marjanovic.png" type="image/png">
+					<img src="assets/images/o-nama/dora-marjanovic.png" alt="Dora Marjanović">
+				</picture>
+				<h4 class="m-t-m m-b-xs">Dora Marjanović</h4>
+				<p class="fg-secondary fs-small">Učitelj edukacijski rehabilitator u produženom stručnom postupku i rehabilitacijskim programima</p>
+			</div>
+			<div class="col-3 col-m-4 col-s-6 col-xs-12 p-b-l ta-center">
+				<picture>
+					<source srcset="assets/images/o-nama/vedrana-jugovic.webp" type="image/webp">
+					<source srcset="assets/images/o-nama/vedrana-jugovic.png" type="image/png">
+					<img src="assets/images/o-nama/vedrana-jugovic.png" alt="Vedrana Jugović">
+				</picture>
+				<h4 class="m-t-m m-b-xs">Vedrana Jugović</h4>
+				<p class="fg-secondary fs-small">Učitelj edukacijski rehabilitator u produženom stručnom postupku i rehabilitacijskim programima</p>
+			</div>
+			<div class="col-3 col-m-4 col-s-6 col-xs-12 p-b-l ta-center">
+				<picture>
+					<source srcset="assets/images/o-nama/gabrijela-hajba.webp" type="image/webp">
+					<source srcset="assets/images/o-nama/gabrijela-hajba.png" type="image/png">
+					<img src="assets/images/o-nama/gabrijela-hajba.png" alt="Gabrijela Hajba">
+				</picture>
+				<h4 class="m-t-m m-b-xs">Gabrijela Hajba</h4>
+				<p class="fg-secondary fs-small">Edukacijski rehabilitator u vrtićkim skupinama</p>
+			</div>
+			<div class="col-3 col-m-4 col-s-6 col-xs-12 p-b-l ta-center">
+				<picture>
+					<source srcset="assets/images/o-nama/kristina-papic.webp" type="image/webp">
+					<source srcset="assets/images/o-nama/kristina-papic.png" type="image/png">
+					<img src="assets/images/o-nama/kristina-papic.png" alt="Kristina Papić">
+				</picture>
+				<h4 class="m-t-m m-b-xs">Kristina Papić</h4>
+				<p class="fg-secondary fs-small">Edukacijski rehabilitator u vrtićkim skupinama</p>
+			</div>
+			<div class="col-3 col-m-4 col-s-6 col-xs-12 p-b-l ta-center">
+				<picture>
+					<source srcset="assets/images/o-nama/marina-hosjak.webp" type="image/webp">
+					<source srcset="assets/images/o-nama/marina-hosjak.png" type="image/png">
+					<img src="assets/images/o-nama/marina-hosjak.png" alt="Marina Hosjak">
+				</picture>
+				<h4 class="m-t-m m-b-xs">Marina Hosjak</h4>
+				<p class="fg-secondary fs-small">Učitelj glazbene kulture</p>
+			</div>
+			<div class="col-3 col-m-4 col-s-6 col-xs-12 p-b-l ta-center">
+				<picture>
+					<source srcset="assets/images/o-nama/tamara-kreso.webp" type="image/webp">
+					<source srcset="assets/images/o-nama/tamara-kreso.png" type="image/png">
+					<img src="assets/images/o-nama/tamara-kreso.png" alt="Tamara Krešo">
+				</picture>
+				<h4 class="m-t-m m-b-xs">Tamara Krešo</h4>
+				<p class="fg-secondary fs-small">Učitelj likovne kulture</p>
+			</div>
+			<div class="col-3 col-m-4 col-s-6 col-xs-12 p-b-l ta-center">
+				<picture>
+					<source srcset="assets/images/o-nama/kristina-gabelica.webp" type="image/webp">
+					<source srcset="assets/images/o-nama/kristina-gabelica.png" type="image/png">
+					<img src="assets/images/o-nama/kristina-gabelica.png" alt="Kristina Gabelica">
+				</picture>
+				<h4 class="m-t-m m-b-xs">Kristina Gabelica</h4>
+				<p class="fg-secondary fs-small">Učitelj tjelesne i zdravstvene kulture</p>
+			</div>
+		</section>
+	</div>
+</section>
+
+<!--CTA-section-->
+<section class="cta-section m-y-l">
+	<div class="cta-section-content">
+		<h1 class="cta-section-content-title">Zanima vas volontiranje?</h1>
+		<p class="cta-section-content-subtitle">Pošaljite nam email na<br><a class="fw-bold" href="mailto:ured@centar-autizam-os.skole.hr">ured@centar-autizam-os.skole.hr</a><br>ili nas nazovite na <a class="fw-bold" href="tel:+385031202327">031/202-327</a></p>
+	</div>
+	<img class="cta-section-image-left" src="assets/images/cta-left.svg" alt="" aria-hidden="true">
+	<img class="cta-section-image-right" src="assets/images/cta-right.svg" alt="" aria-hidden="true">
+</section>
+
+<!--Povijest centra-->
+<section class="grid p-y-xl">
+	<div class="col-6 col-m-8 col-s-10 col-xs-12">
+		<div class="read-format">
+			<h1>Povijest centra</h1>
+			<h3>16. svibnja 2015.</h3>
+			<p>Na sastanku pročelnice Upravnog odjela za prosvjetu, kulturu, šport i tehničku kulturu Osječko-baranjske županije i defektologinje Sande Zlatarić predstavljena je ideja da se Škola za osposobljavanje i obrazovanje „Vinko Bek“ pretvori u Centar za autizam.</p>
+			<h3>18. svibnja 2015.</h3>
+			<p>Sanda Zlatarić, profesor rehabilitator, izabrana je za vršitelja dužnosti ravnatelja u Školi za osposobljavanje i obrazovanje „Vinko Bek“ te 19. svibnja 2015. preuzima navedenu dužnost.</p>
+			<h3>29. svibnja 2015.</h3>
+			<p>Ravnateljica Sanda Zlatarić šalje dopis Upravnom odjelu za prosvjetu, kulturu, šport i tehničku kulturu Osječko-baranjske županije s prijedlogom rješenja o pokretanju Centra za autizam koji je proslijeđen tadašnjem županu Vladimiru Šišljagiću.</p>
+			<h3>4. lipnja 2015.</h3>
+			<p>Na poziv ravnateljice, roditelji iz Inicijative za izgradnju Centra za autizam – Emilija Šmit Polić, Klaudija Scaggs i Daliborka Pavošević obilaze prostor Škole te su suglasni da zajedničkim snagama krenu u borbu za otvaranje budućeg Centra za autizam u Slavoniji, uz podršku osnivača škole Osječko-baranjske županije.</p>
+			<h3>19. lipnja 2015.</h3>
+			<p>U Upravnom odjelu za prosvjetu, kulturu, šport i tehničku kulturu Osječko-baranjske županije održan je radni sastanak na kojem su dogovoreni koraci za realizaciju ideje otvaranja Centra za autizam.</p>
+			<h3>26. lipnja 2015.</h3>
+			<p>U prostoru Škole održan je sastanak na temu broja djece i odraslih korisnika s autizmom koji bi bili polaznici budućeg Centra za autizam.</p>
+			<h3>29. lipnja 2015.</h3>
+			<p>U Upravnom odjelu za prosvjetu, kulturu, šport i tehničku kulturu Osječko-baranjske županije održan je sastanak s temom osnivanja Centra za autizam u sklopu Škole za osposobljavanje i obrazovanje „Vinko Bek“ u Osijeku.</p>
+			<h3>6. srpnja 2015.</h3>
+			<p>U Gradu Osijeku, u prostorijama Velike vijećnice održan je sastanak na temu rješavanja problematike djece i odraslih osoba s autizmom u Osijeku i regiji.</p>
+			<h3>22. rujna 2015.</h3>
+			<p>U prostorijama Upravnog odjela za prosvjetu, kulturu, šport i tehničku kulturu Osječko-baranjske županije održan je sastanak u svezi arhitektonskog rješenja nove zgrade Centra za autizam.</p>
+			<h3>10. listopada 2015.</h3>
+			<p>Između tri ministarstva, županije osječko-baranjske i Grada Osijeka potpisan je Sporazum o suradnji u uspostavi Centra za odgoj, obrazovanje i rehabilitaciju Osijek.</p>
+			<h3>16. prosinca 2015.</h3>
+			<p>Potpisan je dodatak Sporazumu.</p>
+			<h3>17. prosinca 2015.</h3>
+			<p>Osnivač Škole donosi odluku o promjeni naziva i djelatnosti Škole za osposobljavanje i obrazovanje „Vinko Bek“ u  Osijeku i upućuje je MZOS-u.</p>
+			<p>Tijekom mjeseca, ravnateljica Sanda Zlatarić izrađuje Studij opravdanosti izgradnje zgrade Centra za odgoj, obrazovanje i rehabilitaciju Osijek. U međuvremenu MZOS traži doradu Odluke o promjeni imena i djelatnosti škole te isto šalje Osnivaču.</p>
+			<h3>20. srpnja 2016.</h3>
+			<p>Osnivač donosi Rješenje o javnoj nabavi izgradnje Centra za odgoj, obrazovanje i rehabilitaciju Osijek.</p>
+			<h3>26. kolovoza 2016.</h3>
+			<p>Javno otvaranje ponuda.</p>
+			<h3>7. rujna 2016.</h3>
+			<p>Poništenje postupka javne nabave za izgradnju Centra za odgoj, obrazovanje i rehabilitaciju Osijek.</p>
+			<h3>30. rujna 2016.</h3>
+			<p>Na zahtjev župana Vladimira Šišljagića traže se pojašnjenje kvadrature te zakonske odrednice zadanog prostora budućeg centra. Ravnateljica izrađuje studiju prostora.</p>
+			<h3>5. listopada 2016.</h3>
+			<p>Održana je skupština Osječko-baranjske županije  na kojoj je donesena odluka o pokretanju javne nabave za izvođače radova.</p>
+			<h3>18. listopada 2016.</h3>
+			<p>U Županijskom glasniku broj 7/16 objavljena je Odluka o promjeni imena i djelatnosti Škole za osposobljavanje i obrazovanje „Vinko Bek“ Osijek. Odluka postaje pravomoćna 26. listopada 2016. godine.</p>
+			<h3>31. listopada  2016.</h3>
+			<p>Objavljen je natječaj za izgradnju Centra za odgoj obrazovanje i rehabilitaciju Osijek koji je otvoren do 21. studenog 2016. godine. Po nalogu Odjela za prosvjetu Osječko-baranjske županije Škola je, uz suglasnost Školskog odbora, otvorila natječaj za odabir nadzora građenja.</p>
+			<h3>17. siječnja 2017.</h3>
+			<p>Potpisan je ugovor s odabranim izvođačem za izgradnju Centra, tvrtkom ZUBER DOO. Predviđeni je rok radova 2 godine i 5 mjeseci uz mogućnost rebalansa i ranijeg završetka. Uredbom župana vođenje investicije izgradnje Centra preuzeo je Odjel za gospodarstvo i regionalni razvoj Osječko-baranjske županije te je 24. siječnja 2017. godine održan sastanak koordinacije predstavnika Odjela za gospodarstvo i regionalni razvoj Osječko-baranjske županije, nadzora i ravnateljice Centra.</p>
+			<h3>19. stunenog 2018.</h3>
+			<p>Nakon završetka izgradnje Centra obavljen je tehnički pregled građevine te je 19. studenog 2018. godine izdana Uporabna dozvola za istu građevinu. Nakon toga započelo se sa opremanjem Centra. Skupština Osječko-baranjske županije  na sjednici održanoj 13. prosinca 2018. godine donijela je Odluku o promjeni naziva i djelatnosti Centra za odgoj, obrazovanje i rehabilitaciju, a  Ministarstvo znanosti i obrazovanja je 22. siječnja 2019. godine donijelo Rješenje o  sukladnosti ove odluke sa zakonom. Ovom odlukom novi naziv centra je Centra za autizam. Ministarstvo znanosti i obrazovanja je 20. veljače 2019. godine izdalo Rješenje da centar za autizam, Osijek ispunjava uvjete propisane zakonom za rad u promijenjenim uvjetima u novoizgrađenom objektu.</p>
+			<p>Nakon dobivanja potrebnih suglasnosti za 22 radna mjesta od Ministarstva znanosti i obrazovanja, tijekom veljače 2019. god., provodi se natječajni postupak za nova radna mjesta i vrši se zapošljavanje prema provedenom natječajnom postupku.</p>
+			<h3>01. ožujka 2019.</h3>
+			<p>Od 01. ožujka 2019. godine Centar za autizam započinje s radom u okviru djelatnosti vezanih za rad s djecom i učenicima s poremećajima iz spektra autizma. Tijekom veljače i ožujka 2019. godine u tijeku je izrada i usvajanje Statuta Centra za autizam. Skupština Osječko-baranjske županije  na sjednici održanoj 04. travnja 2019. godine donijela je Odluku o davanju prethodne suglasnosti na Statut Centra za autizam, Osijek, slijedom koje je Školski odbor Centra donio Odluku o usvajanju Statuta Centra za autizam, Osijek.</p>
+			<h3>29. travnja 2019.</h3>
+			<p>Nakon stupanja na snagu Statuta , trgovačkom sudu u Osijeku ponesen je zahtjev za promjenu naziva i djelatnosti. Rješenjem Trgovačkog suda u Osijeku od 29. travnja 2019. godine (Tt-19/2638-2) Centar za odgoj, obrazovanje i rehabilitaciju promijenio je naziv u Centar za autizam.</p>
+		</div>
+	</div>
+</section>
