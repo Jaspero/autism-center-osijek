8ae549a3ae011cb7e466a5b9a901c87991b15b07
siclol <marko.milky@gmail.com>
2019-08-09 11:10:43 +0200

    feat(novosti): done?

--- src/routes/novosti.svelte
+++ src/routes/novosti.svelte
@@ -2,4 +2,69 @@
 	<title>CZA Osijek | Novosti</title>
 </svelte:head>
 
-<p>novosti</p>
+<!--Ostale novosti-->
+<section class="grid p-y-xl">
+	<div class="col-12">
+		<h3 class="title m-b-l">E-glas edukacija - Grid 3</h3>
+	</div>
+	<div class="col-6 col-s-12 col-xs-12 read-format">
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+	<div class="col-1 col-s-0"></div>
+	<div class="col-5 col-s-12 col-xs-12">
+		<img class="w-full" src="assets/images/placeholder.png" alt="">
+	</div>
+</section>
+
+<!--Ostale novosti-->
+<section class="grid p-y-xl">
+	<div class="col-12">
+		<h3 class="title m-b-l">Ostale novosti</h3>
+	</div>
+	<div class="col-12">
+		<div class="grid jc-start">
+			<a href="." class="col-3 col-m-6 col-xs-12 card">
+				<p class="fs-small fg-secondary p-x-s">21.09.2019.</p>
+				<h3 class="p-x-s p-b-s">Novi natječaj za posao</h3>
+				<img class="card-image" src="assets/images/placeholder.png" alt="">
+			</a>
+			<a href="." class="col-3 col-m-6 col-xs-12 card">
+				<p class="fs-small fg-secondary p-x-s">21.09.2019.</p>
+				<h3 class="p-x-s p-b-s">Novi natječaj za posao</h3>
+				<img class="card-image" src="assets/images/placeholder.png" alt="">
+			</a>
+			<a href="." class="col-3 col-m-6 col-xs-12 card">
+				<p class="fs-small fg-secondary p-x-s">21.09.2019.</p>
+				<h3 class="p-x-s p-b-s">Novi natječaj za posao</h3>
+				<img class="card-image" src="assets/images/placeholder.png" alt="">
+			</a>
+			<a href="." class="col-3 col-m-6 col-xs-12 card">
+				<p class="fs-small fg-secondary p-x-s">21.09.2019.</p>
+				<h3 class="p-x-s p-b-s">Novi natječaj za posao</h3>
+				<img class="card-image" src="assets/images/placeholder.png" alt="">
+			</a>
+			<a href="." class="col-3 col-m-6 col-xs-12 card">
+				<p class="fs-small fg-secondary p-x-s">21.09.2019.</p>
+				<h3 class="p-x-s p-b-s">Novi natječaj za posao</h3>
+				<img class="card-image" src="assets/images/placeholder.png" alt="">
+			</a>
+			<a href="." class="col-3 col-m-6 col-xs-12 card">
+				<p class="fs-small fg-secondary p-x-s">21.09.2019.</p>
+				<h3 class="p-x-s p-b-s">Novi natječaj za posao</h3>
+				<img class="card-image" src="assets/images/placeholder.png" alt="">
+			</a>
+			<a href="." class="col-3 col-m-6 col-xs-12 card">
+				<p class="fs-small fg-secondary p-x-s">21.09.2019.</p>
+				<h3 class="p-x-s p-b-s">Novi natječaj za posao</h3>
+				<img class="card-image" src="assets/images/placeholder.png" alt="">
+			</a>
+			<a href="." class="col-3 col-m-6 col-xs-12 card">
+				<p class="fs-small fg-secondary p-x-s">21.09.2019.</p>
+				<h3 class="p-x-s p-b-s">Novi natječaj za posao</h3>
+				<img class="card-image" src="assets/images/placeholder.png" alt="">
+			</a>
+		</div>
+	</div>
+</section>
