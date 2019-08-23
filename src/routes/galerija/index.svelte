a137edb0faec99a85f207e705a0f26743df1662b
siclol <marko.milky@gmail.com>
2019-08-23 19:52:02 +0200

    chore: adjusted gallery

--- src/routes/galerija/index.svelte
+++ src/routes/galerija/index.svelte
@@ -12,12 +12,6 @@
 </script>
 
 <section class="grid generic-section">
-	<div class="col-3 col-m-4 col-s-10 col-xs-12">
-		<BlogNavigation labelValue="Galerije:">
-			<BlogNavigationItem isTitle textValue={"G1"} hrefValue={"/galerija"}></BlogNavigationItem>
-			<BlogNavigationItem isTitle textValue={"G2"} hrefValue={"/galerija"}></BlogNavigationItem>
-		</BlogNavigation>
-	</div>
 	<div class="col-6 col-m-8 col-s-10 col-xs-12">
 		<BlogArticle>
 			<Title>Galerija 1</Title>
@@ -56,5 +50,4 @@
 			</div>
 		</BlogArticle>
 	</div>
-	<div class="col-3 col-m-0"></div>
 </section>
